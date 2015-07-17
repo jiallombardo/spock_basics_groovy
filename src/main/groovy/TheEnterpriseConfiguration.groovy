8b1b2d9194d798bf0fdb17d71b638f16110f09de
oleh_lunin <olunin@smartling.com>
2015-07-17 15:46:23 +0300

    Create Proxy for ClearanceProvider via Map coercion.

--- src/main/groovy/TheEnterpriseConfiguration.groovy
+++ src/main/groovy/TheEnterpriseConfiguration.groovy
@@ -2,10 +2,6 @@ import groovy.transform.PackageScope
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-import java.lang.reflect.InvocationHandler
-import java.lang.reflect.Method
-import java.lang.reflect.Proxy
-
 @Configuration
 class TheEnterpriseConfiguration
 {
@@ -17,16 +13,6 @@ class TheEnterpriseConfiguration
     @PackageScope
     @Bean
     TheEnterprise theShip() {
-        def handler = new InvocationHandler() {
-            @Override
-            Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                null
-            }
-        }
-        def proxyProvider = Proxy.newProxyInstance(ClearanceProvider.classLoader,
-                [ClearanceProvider] as Class[],
-                handler)
-
-        new TheEnterprise("trainee", proxyProvider)
+        new TheEnterprise("trainee", [ getClearance : { title -> 'UNAUTHORIZED' as Clearance } ] as ClearanceProvider)
     }
 }
